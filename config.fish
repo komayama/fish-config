9a6e5c5e0320d514e3a410e92beeebabaf65ff0f
KoheiMayama <mym.kohei@gmail.com>
2020-03-04 11:34:08 +0900

    chage contents

--- config.fish
+++ config.fish
@@ -1,1 +1,17 @@
-config.fish
+# peco( suggestion previous commands)
+function fish_user_key_bindings
+    bind \cr peco_select_history
+end
+
+# branch display
+set -g theme_display_git_master_branch yes
+
+# directory full path
+set -g fish_prompt_pwd_dir_length 0
+
+# anyenv
+set -x PATH ~/.anyenv/bin $PATH
+anyenv init - fish | source
+
+# Directory color
+export LSCOLORS=fxfxcxdxbxegedabagacad
